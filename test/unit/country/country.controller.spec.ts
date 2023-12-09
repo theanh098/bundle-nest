d5cdd03bfc7a3f453aa33fcabcd710e98575e62b
theanh098 <mtheanh2910@gmail.com>
2023-12-09 16:34:11 +0700

    update

--- test/unit/country/country.controller.spec.ts
+++ test/unit/country/country.controller.spec.ts
@@ -2,7 +2,7 @@ import { Test } from "@nestjs/testing";
 import { Effect } from "effect";
 import { afterEach, beforeAll, expect, it, vi, describe } from "vitest";
 
-import type { Country } from "@root/shared/IO/Country";
+import type { Country } from "@root/shared/IO/Country.io";
 
 import { AppModule } from "@root/app.module";
 import { CountryController } from "@root/country/country.controller";
